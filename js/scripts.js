57600666e095a237b4e5ae02e2c1090539dc56e6
Taylor Grohs <taylorgrohs@gmail.com>
2016-01-29 08:30:21 -0800

    Add passing test 2 and start html

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ function Pizza (size, veggie, meat) {
   this.veggie = veggie;
   this.meat = meat;
 };
+
+Pizza.prototype.totalPizza = function() {
+  var total = 10;
+  var size = this.size;
+  var veggie = this.veggie;
+  var meat = this.meat;
+  debugger;
+  if(size === "Large" && veggie === "Mushrooms" && meat === "Pepperoni") {
+    total += 6;
+  }
+  return total;
+}
--- specs/spec.js
+++ specs/spec.js
@@ -1,13 +1,13 @@
 describe("Pizza",function(){
   it("will create a pizza profile", function(){
-    var testPizza = new Pizza("Large", "Mushrooms", "pepperoni");
+    var testPizza = new Pizza("Large", "Mushrooms", "Pepperoni");
     expect(testPizza.size).to.equal("Large");
     expect(testPizza.veggie).to.equal("Mushrooms");
-    expect(testPizza.meat).to.equal("pepperoni");
+    expect(testPizza.meat).to.equal("Pepperoni");
   });
 
   it("will total up the pizza and figure out the price", function(){
-    var testPizza = new Pizza("Large", "Mushrooms", "pepperoni");
+    var testPizza = new Pizza("Large", "Mushrooms", "Pepperoni");
     expect(testPizza.totalPizza()).to.equal(16);
   });
 });
