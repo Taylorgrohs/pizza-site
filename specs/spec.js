e6268884d3a6aeab8febedbe2a338f7ee296ecd2
Taylor Grohs <taylorgrohs@gmail.com>
2016-01-29 08:03:08 -0800

    Add failing test 2

--- specs/spec.js
+++ specs/spec.js
@@ -5,4 +5,9 @@ describe("Pizza",function(){
     expect(testPizza.veggie).to.equal("Mushrooms");
     expect(testPizza.meat).to.equal("pepperoni");
   });
+
+  it("will total up the pizza and figure out the price", function(){
+    var testPizza = new Pizza("Large", "Mushrooms", "pepperoni");
+    expect(testPizza.totalPizza()).to.equal(16);
+  });
 });
